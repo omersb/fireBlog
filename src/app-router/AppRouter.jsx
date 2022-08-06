14662606be66586d0614b918137725163fc17641
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-08-06 16:37:13 +0300

    fireblog

---
+++ src/app-router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
